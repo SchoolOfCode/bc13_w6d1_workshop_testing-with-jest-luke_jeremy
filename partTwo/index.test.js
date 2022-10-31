9359e8969701d9b08c0901107fad2d3b85aefbe1
luke123adams <lxa207@gmail.com>
2022-10-31 19:28:28 +0000

    added tests for doubleAndAddOne, isLongArray

--- partTwo/index.test.js
+++ partTwo/index.test.js
@@ -39,4 +39,19 @@ test(`When given a boolean instead of a string, the greetPerson function returns
   expect(actual).toBe(expected);
 });
 
+
+
 //👉 2.2 - Write your tests for the other functions below:
+test(`When 1 is given as an argument, doubleAndAddOne =3`, () => {
+  const actual = doubleAndAddOne(1);
+  const expected = 3;
+
+  expect(actual).toBe(expected);
+});
+
+test('When an array is longer than 10, return true ', () => {
+  const actual = isLongArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+  const expected = true;
+
+   expect(actual).toBe(expected);
+})
